500848683a0acd01850249bc8ae9d08ee14551a1
Vietcoder <sirdang@Macintoshs-MacBook.local>
2022-11-05 21:44:07 +0700

    doen

--- djang_website/settings.py
+++ djang_website/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -211,5 +212,5 @@ TINYMCE_DEFAULT_CONFIG = {
     "height": "600px",
     'image_caption': True,
     "images_upload_url": "upload_image",
-}import django_heroku
-django_heroku.settings(locals())
+}
+
